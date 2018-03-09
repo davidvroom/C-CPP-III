85ceb9775ae7cf188bf6d03db20481cab9d2a4e1
Unknown <klaasisaac_bijlsma@hotmail.com>
2018-03-09 12:55:39 +0100

    ex 24 en 25 af

---
+++ week_4/ex24/insertable.h
@@ -0,0 +1,22 @@
+#ifndef INCLUDED_INSERTABLE_H_	
+#define INCLUDED_INSERTABLE_H_
+
+#include <ostream>
+
+template <template <typename> class Container, typename Type>
+struct Insertable: public Container<Type>
+{
+	Insertable() = default;
+	explicit Insertable(Container<Type> const &initCont);
+	explicit Insertable(Type const &initDataEl);
+};
+
+template<template <typename> class Container, typename Type>
+std::ostream &operator<<(std::ostream &out, Insertable<Container, Type> const &obj)
+{
+	for (size_t idx = 0, end = obj.size(); idx != end; ++idx)
+		out << obj[idx] << '\n';
+	return out;
+}
+
+#endif
---
+++ week_4/ex26/traits.h
@@ -0,0 +1,1 @@
+#ifndef INCLUDED_T
