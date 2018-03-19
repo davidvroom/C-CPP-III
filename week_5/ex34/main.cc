ab0f94f071e7d47d937a3ca95de983c2d76908af
Unknown <davidvroom93@hotmail.com>
2018-03-19 22:43:44 +0100

    haii

--- week_5/ex34/main.cc
+++ week_5/ex34/main.cc
@@ -1,13 +1,10 @@
 #include "scanner/scanner.h"
-#include <fstream>
-#include <string>
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-					// replace NTBS by argv[1], but this is easier for testing
-	Scanner scanner{"input.txt"};	
+	Scanner scanner{ argv[1] };	
 	scanner.lex();
 	scanner.finish();
 }
